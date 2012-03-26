dc876922f4018b81fe8b607b958c28e47774c90a
Andrew Gerst <gerst20051@gmail.com>
2012-03-26 19:51:10 -0400

    improved switch navigation

--- src/application.js
+++ src/application.js
@@ -4,7 +4,7 @@ dojo.require('dojo.window');
 dojo.require('dojo.io.script');
 
 dojo.ready(function(){
-	app = new youtube.Main();
+	window.app = new youtube.Main();
 });
 
 dojo.declare('youtube.Main', null, {
@@ -12,6 +12,8 @@ dojo.declare('youtube.Main', null, {
 		"devkey": "AI39si61JkTRRLScpnvH9VvPq4iTVsg0O15u5brhMLiDw6T_OES9rgaJ43fU9rBXQyU3OdVXXdqNU3Yn249xey7ygHFKYTdSOQ",
 		"title": "Accessible YouTube",
 		"vThumbs": 5,
+		"vThumbsMax": 25,
+		"vThumbsMaxPages": 5,
 		"playerWidth": 720,
 		"playerHeight": 405,
 		"thumbHeight": 99,
@@ -22,13 +24,15 @@ dojo.declare('youtube.Main', null, {
 		"currentPlaylistPage": 1,
 		"currentPlaylistPos": 1,
 		"selectedPlaylistPos": 1,
+		"currentMenuPos": 1,
 		"videoSelected": false,
+		"controls": ["pause","change","beginning","back","forward","related"],
 		"playerState": -1,
 		"playlistArr": [],
 		"vs": {}
 	},
 	constructor: function(){
-		this.handleScreenSize();
+		this.handleDimensions();
 		this.loadPlayer();
 
 		dojo.query('.home-link').connect('onclick', this, function(){
@@ -37,6 +41,7 @@ dojo.declare('youtube.Main', null, {
 
 		dojo.connect(dojo.byId('most_popular-link'), 'onclick', this, function(){
 			this.showPage('main');
+			this.getVideos('popular');
 		});
 
 		dojo.connect(dojo.byId('options-link'), 'onclick', this, function(){
@@ -61,49 +66,38 @@ dojo.declare('youtube.Main', null, {
 		});
 		
 		dojo.subscribe('/video-list/loaded', function(){
-			dojo.query('#video-list li').connect('onmouseenter', function(e){
-				/*
+			dojo.query('#video-list li').connect('onclick', function(e){
 				var vid = dojo.attr(this, "id"), index = -1;
-				if (vid) {
-					if (vid == app.aC.currentVideoId) return;
-					if (!app.aC.videoSelected) app.loadVideo(vid);
-					dojo.forEach(app.aC.playlistArr, function(v,i){
-						if (index > -1) return;
-						if (v.id == vid) index = i;
-					});
-					dojo.query('#video-list li').removeClass('selected');
-					dojo.query('#video-list li:nth-child('+(index+2)+')').addClass('selected');
-				} else {
-					var cname = dojo.attr(this, "className");
-					dojo.query('#video-list li').removeClass('selected');
-					if (cname == "leftarrow") dojo.query('#video-list li:nth-child(1)').addClass('selected');
-					else if (cname == "rightarrow") dojo.query('#video-list li:nth-child('+(app.aC.vThumbs+2)+')').addClass('selected');
-				}
-				*/
-			}).connect('onclick', function(e){
-				var vid = dojo.attr(this, "id");
-				if (vid) {
-					if (vid == app.aC.currentVideoId) return;
-					if (!app.aC.videoSelected) {
-						app.loadVideo(vid);
-						app.aC.videoSelected = true;
+				if (vid) { 
+					if (vid == window.app.aC.currentVideoId) return;
+					window.app.loadVideo(vid);
+					window.app.aC.videoSelected = true;
+					var i = 0, begin = ((window.app.aC.currentPlaylistPage - 1) * window.app.aC.vThumbs), end = (window.app.aC.currentPlaylistPage * window.app.aC.vThumbs);
+					for (i = begin; i < end; i++) {
+						if (index > -1) break;
+						if (window.app.aC.playlistArr[i].id == vid) index = i;
 					}
-					dojo.forEach(app.aC.playlistArr, function(v,i){
-						if (index > -1) return;
-						if (v.id == vid) index = i;
-					});
+					window.app.aC.currentPlaylistPos = index;
+					console.log('begin',begin,'end',end,'pos',window.app.aC.currentPlaylistPos);
 					dojo.query('#video-list li').removeClass('selected');
-					dojo.query('#video-list li:nth-child('+(index+2)+')').addClass('selected');
+					dojo.query('#video-list li:nth-child('+(window.app.aC.currentPlaylistPos+2)+')').addClass('selected');
 					dojo.removeClass("control-list","inactive");
+					window.app.yt.playVideo();
 				} else {
 					var cname = dojo.attr(this, "className").split(" ")[0];
 					if (cname == "leftarrow") {
-						if (app.aC.currentPlaylistPage > 1) {
-							alert("Back: Current Page = " + (--app.aC.currentPlaylistPage));
+						if (window.app.aC.currentPlaylistPage > 1) {
+							dojo.query('#video-list li').removeClass('selected');
+							dojo.query('#video-list li:nth-child(2)').addClass('selected');
+							--window.app.aC.currentPlaylistPage;
+							window.app.displayVideos();
 						}
 					} else if (cname == "rightarrow") {
-						if (app.aC.currentPlaylistPage < 5) {
-							alert("Next: Current Page = " + (++app.aC.currentPlaylistPage));
+						if (window.app.aC.currentPlaylistPage < 5) {
+							dojo.query('#video-list li').removeClass('selected');
+							dojo.query('#video-list li:nth-child(2)').addClass('selected');
+							++window.app.aC.currentPlaylistPage;
+							window.app.displayVideos();
 						}
 					}
 				}
@@ -111,33 +105,51 @@ dojo.declare('youtube.Main', null, {
 		});
 		
 		dojo.connect(window, "onkeydown", this, function(e){
-			switch(e.keyCode){
-				case dojo.keys.LEFT_ARROW:
-				case dojo.keys.UP_ARROW:
-					if (this.aC.videoSelected) {
-						// do selected control
-						// if it's choose another movie
-						// - deselect control list and switch to video selection
-						console.log(e.target.id);
-						
-						dojo.addClass("control-list","inactive");
-					} else {
-						this.aC.videoSelected = true;
-						dojo.removeClass("control-list","inactive");
-						this.loadVideo(this.aC.selectedVideoId);
-						this.aC.currentVideoId = this.aC.selectedVideoId;
-						// play selected video
-						// switch to control list
-					}
-				break;
-				case dojo.keys.RIGHT_ARROW:
-				case dojo.keys.DOWN_ARROW:
-					if (this.aC.videoSelected) {
-						// move to next item in control list
-					} else {
-						// move to next video
-					}
-				break;
+			if (this.aC.currentPage == "main") {
+				switch(e.keyCode){
+					case dojo.keys.LEFT_ARROW:
+					case dojo.keys.UP_ARROW:
+						if (this.aC.videoSelected) { // do selected control
+							dojo.addClass("control-list","inactive");
+							this.controlList(id);
+						} else {
+							// play selected video or next page
+							// switch to control list
+							if (this.aC.currentPlaylistPos == 0) {
+								--this.aC.currentPlaylistPage;
+								this.displayVideos();
+							} else if (this.aC.currentPlaylistPos == (this.aC.vThumbs + 2)) {
+								++this.aC.currentPlaylistPage;
+								this.displayVideos();
+							} else {
+								this.aC.videoSelected = true;
+								dojo.removeClass("control-list","inactive");
+								var vIndex = ((this.aC.currentPlaylistPage - 1) * this.aC.vThumbs) + this.aC.currentPlaylistPos;
+								this.aC.currentVideoId = this.aC.playlistArr[vIndex].id;
+								this.loadVideo(this.aC.currentVideoId);
+								this.yt.playVideo();
+							}
+						}
+					break;
+					case dojo.keys.RIGHT_ARROW:
+					case dojo.keys.DOWN_ARROW:
+						if (this.aC.videoSelected) { // move to next item in control list
+							if (++this.aC.currentMenuPos == 6) this.aC.currentMenuPos = 0;
+							dojo.query('#control-list li').removeClass('selected');
+							dojo.query('#control-list li:nth-child('+(this.aC.currentMenuPos+1)+')').addClass('selected');
+						} else { // move to next video
+							if (++this.aC.currentPlaylistPos == (this.aC.vThumbs + 2)) this.aC.currentPlaylistPos = 0;
+							dojo.query('#video-list li').removeClass('selected');
+							dojo.query('#video-list li:nth-child('+(this.aC.currentPlaylistPos+1)+')').addClass('selected');
+						}
+					break;
+				}
+			} else if (this.aC.currentPage == "splash") {
+				switch(e.keyCode){
+					case dojo.keys.ENTER:
+						dojo.byId('b_search').click();
+					break;
+				}
 			}
 		});
 	},
@@ -152,19 +164,8 @@ dojo.declare('youtube.Main', null, {
 		var b = {id: "ytplayer"};
 		swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=ytplayer&key="+this.aC.devkey, "iVD", this.aC.playerWidth, this.aC.playerHeight, "8", null, null, a, b)
 	},
-	getVideos: function(){
-		return dojo.io.script.get({
-			url: "http://gdata.youtube.com/feeds/api/videos",
-			callbackParamName: "callback",
-			content: {
-				"q": this.aC.currentSearch,
-				"format": 5,
-				"max-results": this.aC.vThumbs,
-				"v": 2,
-				"alt": "jsonc"
-			}
-		}).then(function(result){
-			var items = result.data.items,
+	displayVideos: function(){
+			var items = this.aC.playlistArr, ul = dojo.create("ul", {id:"video-list",className:"cf"}, "pW", "only"),
 				typemap = {
 					"Date": {
 						deserialize: function(value){
@@ -176,63 +177,50 @@ dojo.declare('youtube.Main', null, {
 						}
 					}
 				};
-			dojo.subscribe('/player/ready', dojo.hitch(app, function(){
-				this.loadVideo(items[0].id);
-				this.aC.currentVideoId = items[0].id;
-				this.aC.playlistArr = items;
-			}));
-			var ul = dojo.create("ul", {id:"video-list",className:"cf"}, "pW", "only");
 			dojo.create("li", {
 				className: 'leftarrow',
 				innerHTML: '<img src="i/larrow.png"/><div class="info">Back</div>'
 			}, ul);
-			dojo.forEach(items, function(item){
+			var i = 0, begin = ((this.aC.currentPlaylistPage - 1) * this.aC.vThumbs), end = (this.aC.currentPlaylistPage * this.aC.vThumbs);
+			for (i = begin; i < end; i++) {
 				dojo.create("li", {
-					id: item.id,
-					innerHTML: '<img src="'+item.thumbnail.sqDefault+'"/><div class="info">'+item.title+'</div>'
+					id: items[i].id,
+					innerHTML: '<img src="'+items[i].thumbnail.sqDefault+'"/><div class="info">'+items[i].title+'</div>'
 				}, ul);
-			});
+			}
 			dojo.create("li", {
 				className: 'rightarrow',
 				innerHTML: '<img src="i/rarrow.png"/><div class="info">Next</div>'
 			}, ul);
 			dojo.query('#video-list li:nth-child(2)').addClass('selected');
 			dojo.publish('/video-list/loaded');
+	},
+	getVideos: function(){
+		var url = "http://gdata.youtube.com/feeds/api/videos";
+		if (arguments[0] == "popular") url = "http://gdata.youtube.com/feeds/api/standardfeeds/top_favorites";
+		return dojo.io.script.get({
+			url: url,
+			callbackParamName: "callback",
+			content: {
+				"q": this.aC.currentSearch,
+				"max-results": this.aC.vThumbsMax,
+				"v": 2,
+				"alt": "jsonc"
+			}
+		}).then(function(result){
+			var items = result.data.items;
+			dojo.subscribe('/player/ready', dojo.hitch(app, function(){
+				this.loadVideo(items[0].id);
+				this.aC.currentVideoId = items[0].id;
+				this.aC.playlistArr = items;
+				this.displayVideos();
+			}));
 		});
 	},
-	goNextVideo: function(){
-		if (this.aC.currentPlaylistPos == (this.aC.vThumbs - 1)) {
-			this.goVid(0, this.aC.currentPlaylistPage);
-			return;
-		}
-		this.goVid((this.aC.currentPlaylistPos + 1), this.aC.currentPlaylistPage);
-	},
-	goPrevVideo: function(){
-		if (this.aC.currentPlaylistPos == 0) return;
-		this.goVid((this.aC.currentPlaylistPos - 1), this.aC.currentPlaylistPage);
-	},
-	goVid: function(a, b){
-		if (!this.aC.playlistShowing) return;
-		if (b != this.aC.currentPlaylistPage) {
-			this.aC.currentPlaylistPage = b;
-			return;
-		}
-		this.loadAndPlayVideo(this.aC.playlistArr[b][a].id, a);
-	},
-	loadAndPlayVideo: function(a, b, c){
-		if (this.aC.currentPlaylistPos == b) {
-			this.aC.playPause();
-			return;
-		}
-		if (ytplayer) {
-			ytplayer.loadVideoById(a);
-			this.aC.currentVideoId = a;
-		}
-		this.aC.currentPlaylistPos = b;
-	},
-	handleScreenSize: function(){
+	handleDimensions: function(){
 		var vs = dojo.window.getBox();
 		this.aC.vThumbs = (Math.floor((vs.w - 20) / 140) - 2);
+		this.aC.vThumbsMax = this.aC.vThumbsMaxPages * this.aC.vThumbs;
 		this.aC.vs = vs;
 	},
 	loadVideo: function(id){
@@ -240,6 +228,17 @@ dojo.declare('youtube.Main', null, {
 			ytplayer.cueVideoById(id);
 			this.aC.currentVideoId = id;
 		}
+	},
+	goNextVideo: function(){
+		if (this.aC.currentPlaylistPos == this.aC.vThumbs + 2) {
+			this.aC.currentPlaylistPos = 1;
+			this.aC.currentPlaylistPage++;
+			this.displayVideos();
+		}
+		var vIndex = (this.aC.currentPlaylistPage * this.aC.vThumbs) + this.aC.currentPlaylistPos;
+		this.aC.currentVideoId = this.aC.playlistArr[vIndex].id;
+		this.loadVideo(this.aC.currentVideoId);
+		this.yt.playVideo();
 	},
 	playPause: function(){
 		if (ytplayer) {
@@ -274,12 +273,13 @@ dojo.declare('youtube.Main', null, {
 		}
 	},
 	controlList: function(id){
-		alert(id);
+		console.log(id);
 		switch(id){
 			case "pause":
 				
 			break;
 			case "change":
+				this.aC.currentMenuPos = 0;
 				this.aC.videoSelected = false;
 				dojo.addClass("control-list","inactive");
 			break;
@@ -307,14 +307,14 @@ function onYouTubePlayerReady(a){
 }
 
 function onPlayerStateChange(a){
-	app.aC.playerState = a;
-	if (app.aC.playerState == 1) {
-	} else if (app.aC.playerState == 0) {
-		// app.aC.goNextVideo();
+	window.app.aC.playerState = a;
+	if (window.app.aC.playerState == 1) {
+	} else if (window.app.aC.playerState == 0) {
+		window.app.aC.goNextVideo();
 	}
 }
 
 function onPlayerError(a){
-	app.aC.goNextVideo();
+	window.app.aC.goNextVideo();
 	console.log("Error! oPE Type: " + a);
 }
